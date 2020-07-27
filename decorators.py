23996e5bcfaa1438a04980db5a3f41cfe0d88fe5
Sebastian Gnip <travin42@gmail.com>
2020-07-27 21:21:51 +0200

    Decorator class added.

--- decorators.py
+++ decorators.py
@@ -1,8 +1,16 @@
 def decorator_function(original_function):
     def wrapper_function(*args, **kwargs):
-        print('wrapper executed this before {}'.format(original_function.__name__))
+        print('wrapper function executed this before {}'.format(original_function.__name__))
         return original_function(*args, **kwargs)
     return wrapper_function
+
+class decorator_class(object):
+    def __init__(self, original_function):
+        self.original_function = original_function
+
+    def __call__(self, *args, **kwargs):
+        print('class executed this before {}'.format(self.original_function.__name__))
+        self.original_function(*args, **kwargs)
 
 @decorator_function
 def display():
@@ -10,7 +18,18 @@ def display():
 
 @decorator_function
 def display_info(name, age):
-    print('{} ran with arguments ({}, {})'.format(display_info.__name__, name, age))
+    print('display_info ran with arguments ({}, {})'.format(name, age))
+
+display_info('john', 25)
+display()
+
+@decorator_class
+def display():
+    print('display function ran')
+
+@decorator_class
+def display_info(name, age):
+    print('display_info ran with arguments ({}, {})'.format(name, age))
 
 display_info('john', 25)
 display()

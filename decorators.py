4fe65177b5eb2d4b4b3e30b1a71e75d02211b646
Sebastian Gnip <travin42@gmail.com>
2020-07-27 21:55:55 +0200

    A practical example using decorators.

--- decorators.py
+++ decorators.py
@@ -1,35 +1,37 @@
-def decorator_function(original_function):
-    def wrapper_function(*args, **kwargs):
-        print('wrapper function executed this before {}'.format(original_function.__name__))
-        return original_function(*args, **kwargs)
-    return wrapper_function
+from functools import wraps
 
-class decorator_class(object):
-    def __init__(self, original_function):
-        self.original_function = original_function
+def my_logger(orig_func):
+    import logging
+    logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
 
-    def __call__(self, *args, **kwargs):
-        print('class executed this before {}'.format(self.original_function.__name__))
-        self.original_function(*args, **kwargs)
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        logging.info(
+            'Ran with args: {}, and kwargs: {}'.format(args, kwargs))
+        return orig_func(*args, **kwargs)
 
-@decorator_function
-def display():
-    print('display function ran')
+    return wrapper
 
-@decorator_function
+
+def my_timer(orig_func):
+    import time
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print('{} ran in: {} sec'.format(orig_func.__name__, t2))
+        return result
+
+    return wrapper
+
+import time
+
+@my_logger
+@my_timer
 def display_info(name, age):
+    time.sleep(1)
     print('display_info ran with arguments ({}, {})'.format(name, age))
 
-display_info('john', 25)
-display()
-
-@decorator_class
-def display():
-    print('display function ran')
-
-@decorator_class
-def display_info(name, age):
-    print('display_info ran with arguments ({}, {})'.format(name, age))
-
-display_info('john', 25)
-display()
+display_info('Mara', 27)
